f0d3a7d7d38f73f5692fd9c847ccf4b7123820be
ajy7227 <ajy7227@rit.edu>
2018-04-10 14:38:18 -0400

    StationModel implemented.

---
+++ src/student/StationModel.java
@@ -0,0 +1,39 @@
+package student;
+
+import model.Space;
+import model.Station;
+
+public class StationModel implements Station{
+
+    private int row, col;
+    private String name;
+
+    public StationModel(String name, int row, int col){
+        this.row = row;
+        this.col = col;
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getRow() {
+        return row;
+    }
+
+    @Override
+    public int getCol() {
+        return col;
+    }
+
+    @Override
+    public boolean collocated(Space other) {
+        if (other.getCol() == this.col && other.getRow() == this.row){
+            return true;
+        }
+        return false;
+    }
+}
