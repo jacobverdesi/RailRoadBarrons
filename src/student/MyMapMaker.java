79d8f841a1fe517177f27a9b316d0645df396653
Jacob's Laptop <jxv3386@rit.edu>
2018-04-11 14:10:47 -0400

    MapMaker Work

--- src/student/MyMapMaker.java
+++ src/student/MyMapMaker.java
@@ -1,18 +1,43 @@
 package student;
 
-import model.MapMaker;
-import model.RailroadBaronsException;
-import model.RailroadMap;
+import model.*;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.List;
+import java.util.Scanner;
 
 public class MyMapMaker implements MapMaker {
 
     @Override
-    public RailroadMap readMap(InputStream in)throws RailroadBaronsException {
-     return null;
+    public RailroadMap readMap(InputStream in) throws RailroadBaronsException {
+        RailroadMap railroadMap=null;
+        List<Space> spaces;
+        List<Station> stations;
+        List<Track> tracks;
+        List<Route> routes;
+        Scanner scanner = new Scanner(in);
+        Boolean isRoutes=false;
+        while (scanner.hasNextLine()){
+            String s = scanner.nextLine();
+            if(s.equals("##ROUTES##")){
+                isRoutes=true;
+            }
+            else if(!isRoutes){
+                String[] split = s.split(" ");
+
+            }
+            else {
+
+            }
+
+
+        }
+
+        return railroadMap;
     }
+
     @Override
     public void writeMap(RailroadMap map, OutputStream out) {
 
