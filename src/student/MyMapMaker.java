86f4d6aae2d1d70082aacdcaed580a2832a64357
Jacob's Laptop <jxv3386@rit.edu>
2018-04-11 14:21:48 -0400

    MapMaker Work

--- src/student/MyMapMaker.java
+++ src/student/MyMapMaker.java
@@ -5,6 +5,7 @@ import model.*;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -14,7 +15,7 @@ public class MyMapMaker implements MapMaker {
     public RailroadMap readMap(InputStream in) throws RailroadBaronsException {
         RailroadMap railroadMap=null;
         List<Space> spaces;
-        List<Station> stations;
+        List<Station> stations=new ArrayList<>();
         List<Track> tracks;
         List<Route> routes;
         Scanner scanner = new Scanner(in);
@@ -26,7 +27,8 @@ public class MyMapMaker implements MapMaker {
             }
             else if(!isRoutes){
                 String[] split = s.split(" ");
-
+                Station station=new MyStation(split[2],Integer.parseInt(split[0]),Integer.parseInt(split[1]));
+                stations.add(station);
             }
             else {
 
