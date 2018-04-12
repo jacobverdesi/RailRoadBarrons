87f17395eac4910226bbc69b901742f092c75668
Jacob's Laptop <jxv3386@rit.edu>
2018-04-12 16:21:16 -0400

    MapMakerRead done probably

--- src/student/MyMapMaker.java
+++ src/student/MyMapMaker.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
@@ -15,7 +16,7 @@ public class MyMapMaker implements MapMaker {
     public RailroadMap readMap(InputStream in) throws RailroadBaronsException {
         RailroadMap railroadMap = null;
         List<Space> spaces = new ArrayList<>();
-        List<Station> stations = new ArrayList<>();
+        HashMap<Integer,Station> stations = new HashMap<>();
         List<Track> tracks;
         List<Route> routes = new ArrayList<>();
         Scanner scanner = new Scanner(in);
@@ -26,7 +27,7 @@ public class MyMapMaker implements MapMaker {
                 isRoutes = true;
                 int maxCol = 0;
                 int maxRow = 0;
-                for (Station station : stations) {
+                for (Station station : stations.values()) {
                     if (station.getRow() > maxRow) {
                         maxRow = station.getRow();
                     } else if (station.getCol() > maxCol) {
@@ -41,17 +42,17 @@ public class MyMapMaker implements MapMaker {
                 }
             } else if (!isRoutes) {
                 String[] split = s.split(" ");
-                Station station = new MyStation(split[3], Integer.parseInt(split[0]), Integer.parseInt(split[1]), Integer.parseInt(split[2]));
-                stations.add(station);
+                Station station = new MyStation(split[3], Integer.parseInt(split[1]), Integer.parseInt(split[2]));
+                stations.put(Integer.parseInt(split[0]),station);
             } else {
                 String[] split = s.split(" ");
                 Station origin = null, dest = null;
                 Orientation orientation = null;
-                for (Station station : stations) {
-                    if (station.getStationNum == Integer.parseInt(split[0])) {
-                        origin = station;
-                    } else if (station.getStationNum() == Integer.parseInt(split[1])) {
-                        dest = station;
+                for (Integer integer : stations.keySet()) {
+                    if(integer== Integer.parseInt(split[0])) {
+                        origin = stations.get(integer);
+                    }else if(integer== Integer.parseInt(split[0])) {
+                        dest = stations.get(integer);
                     }
                 }
                 if (origin.getCol() == dest.getCol()) {
@@ -75,8 +76,11 @@ public class MyMapMaker implements MapMaker {
                 routes.add(route);
             }
         }
-
-        railroadMap = new MyMap(routes, stations, spaces);
+        List<Station> stationList=new ArrayList<>();
+        for (Integer integer : stations.keySet()) {
+            stationList.add(stations.get(integer));
+        }
+        railroadMap = new MyMap(routes, stationList, spaces);
         return railroadMap;
     }
 
--- src/student/MyStation.java
+++ src/student/MyStation.java
@@ -6,20 +6,13 @@ import model.Station;
 public class MyStation implements Station{
 
     private int row, col;
-    public int stationNum;
     private String name;
 
-    public MyStation(String name,int stationNum, int row, int col){
+    public MyStation(String name,int row, int col){
         this.row = row;
         this.col = col;
         this.name = name;
-        this.stationNum=stationNum;
     }
-
-    public int getStationNum() {
-        return stationNum;
-    }
-
     @Override
     public String getName() {
         return name;
