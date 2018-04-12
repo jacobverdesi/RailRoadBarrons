cffd9295cfa6dfec295e8805d96b57e9365c7337
Jacob's Laptop <jxv3386@rit.edu>
2018-04-12 16:05:47 -0400

    Test class created Map maker work

---
+++ TestSrc/student/MyMapMakerTest.java
@@ -0,0 +1,18 @@
+package student;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MyMapMakerTest {
+    @BeforeClass
+    public static void init(){
+        //initilize fields
+    }
+    @Test
+    public void testReadmap(){
+
+    }
+
+}
--- src/student/MyMapMaker.java
+++ src/student/MyMapMaker.java
@@ -24,19 +24,18 @@ public class MyMapMaker implements MapMaker {
             String s = scanner.nextLine();
             if (s.equals("##ROUTES##")) {
                 isRoutes = true;
-                int maxCol=0;
-                int maxRow=0;
-                for(Station station:stations){
-                    if(station.getRow()>maxRow){
-                        maxRow=station.getRow();
-                    }
-                    else if(station.getCol()>maxCol){
-                        maxCol=station.getCol();
+                int maxCol = 0;
+                int maxRow = 0;
+                for (Station station : stations) {
+                    if (station.getRow() > maxRow) {
+                        maxRow = station.getRow();
+                    } else if (station.getCol() > maxCol) {
+                        maxCol = station.getCol();
                     }
                 }
-                for(int i=0;i<maxCol;i++){
-                    for(int j=0;j<maxCol;j++){
-                        MySpace space=new MySpace(i,j);
+                for (int i = 0; i < maxCol; i++) {
+                    for (int j = 0; j < maxCol; j++) {
+                        MySpace space = new MySpace(i, j);
                         spaces.add(space);
                     }
                 }
@@ -48,20 +47,31 @@ public class MyMapMaker implements MapMaker {
                 String[] split = s.split(" ");
                 Station origin = null, dest = null;
                 Orientation orientation = null;
-                if (==Integer.parseInt(split[0])){
-                    origin = station;
+                for (Station station : stations) {
+                    if (station.getStationNum == Integer.parseInt(split[0])) {
+                        origin = station;
+                    } else if (station.getStationNum() == Integer.parseInt(split[1])) {
+                        dest = station;
+                    }
                 }
-                   else if (station.getStationNum() == Integer.parseInt(split[1])) {
-                    dest = station;
-                }
-
                 if (origin.getCol() == dest.getCol()) {
                     orientation = Orientation.VERTICAL;
                 } else if (origin.getRow() == dest.getRow()) {
                     orientation = Orientation.HORIZONTAL;
-                } else {
                 }
+
                 MyRoute route = new MyRoute(origin, dest, orientation);
+                if (!split[2].equals("UNCLAIMED")) {
+                    if (split[2].equals("Red")) {
+                        route.claim(Baron.RED);
+                    } else if (split[2].equals("Yellow")) {
+                        route.claim(Baron.YELLOW);
+                    } else if (split[2].equals("Blue")) {
+                        route.claim(Baron.BLUE);
+                    } else if (split[2].equals("Green")) {
+                        route.claim(Baron.GREEN);
+                    }
+                }
                 routes.add(route);
             }
         }
